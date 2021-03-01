3ea15d0a4734b8b3342d0ba2935f1c0f6a098501
Gus Narea <gnarea@users.noreply.github.com>
2021-03-01 16:54:43 +0000

    Remove unused TF var

--- tf-workspace/variables.tf
+++ tf-workspace/variables.tf
@@ -6,9 +6,6 @@ variable "gcp_service_account_id" {}
 variable "sre_iam_uri" {
   description = "GCP IAM URI for an SRE or the SRE group (e.g., 'group:sre-team@acme.com')"
 }
-variable "sre_monitoring_notification_channel" {
-  description = "Cloud Monitoring notification channel for the SRE team"
-}
 variable "sre_email_addresses" {
   description = "Email address for each SRE at Relaycorp"
   type        = list(string)
