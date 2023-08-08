df3d26f91440dd2f360129d34fd26c2f7a39c895
Gus Narea <gnarea@users.noreply.github.com>
2023-08-08 17:19:47 +0100

    Upgrade Google and Random providers to versions 4.75 and 3.5 respectively

--- tf-workspace/versions.tf
+++ tf-workspace/versions.tf
@@ -2,16 +2,16 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 3.27"
+      version = "~> 4.75.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 3.27"
+      version = "~> 4.75.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.0.0"
+      version = "~> 3.5.1"
     }
   }
-  required_version = ">= 0.13"
+  required_version = ">= 1.5.3"
 }
