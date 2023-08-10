61d277cc98359f16dc7c222fa36cb580f5abed8c
Gus Narea <gnarea@users.noreply.github.com>
2023-08-10 12:23:42 +0100

    Revert "Temporarily grant myself write access to project"
    This reverts commit af62813a73be6f1cd09043c4111f1241700ee36d.

--- gcp.tf
+++ gcp.tf
@@ -1,10 +1,9 @@
-# TODO: REINSTATE
-#resource "google_project_iam_member" "project_viewer" {
-#  project = var.gcp_project_id
-#
-#  role   = "roles/viewer"
-#  member = var.sre_iam_uri
-#}
+resource "google_project_iam_member" "project_viewer" {
+  project = var.gcp_project_id
+
+  role   = "roles/viewer"
+  member = var.sre_iam_uri
+}
 
 locals {
   services = [
